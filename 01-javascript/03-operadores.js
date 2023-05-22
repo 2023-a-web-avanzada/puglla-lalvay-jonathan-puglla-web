6d79ede06e6226322e2774b95a59b7363747305c
Jonathan Puglla <jonathan.puglla@outlook.com>
2023-05-22 18:34:10 -0500

    Operators in JavaScript
    Operators in JavaScript is done.

--- 01-javascript/03-operadores.js
+++ 01-javascript/03-operadores.js
@@ -15,7 +15,7 @@ const arreglo = [{id: 1, nombre: 'Adrian', nota: 5}, {id: 2, nombre: 'Vicente', 
 
 const respuestaFind = arreglo
     .find(
-        function (valorActual,indiceActual,arregloCompleto) {
+        function (valorActual, indiceActual, arregloCompleto) {
             console.log("valor actual", valorActual);
             console.log("indice actual", indiceActual);
             console.log("arreglo completo", arregloCompleto);
@@ -23,3 +23,112 @@ const respuestaFind = arreglo
         }
     );
 console.log("respuestaFind: ", respuestaFind);  // Adrian
+
+// FINDINDEX
+// enviamos una expresión -> truty falsy
+// devolver el índice del primer elemento que cumpla esa condición
+
+const respuestaFindIndex = arreglo
+    .findIndex(
+        function (varlorActual) {
+            return varlorActual.nota < 5; // Expresión (< =)
+        }
+    );
+console.log('respuestaFindIndex', respuestaFindIndex); // Si encuentra el índice, sino -1
+
+// FOREACH
+// Iterar el arreglo
+const respuestaForEach = arreglo.forEach(
+    function (valorActual) {
+        console.log(valorActual);
+    }
+)
+console.log('respuestaForEach', respuestaForEach); // undefined
+
+// MAP (Modificar o Iterar en un NUEVO arreglo)
+// enviamos los datos del nuevo arreglo
+// devuelve el nuevo arreglo
+const respuestaMap = arreglo
+    .map(
+        function (valorActual) {
+            const nuevaNota = {
+                id: valorActual.id,
+                nombre: valorActual.nombre,
+                nota: valorActual.nota + 1,
+                estaAprobado: (valorActual.nota + 1) > 14,
+                registrado: true,
+            }
+            return nuevaNota;
+        }
+    );
+
+console.log('respuestaMap', respuestaMap); // [...] nuevo arreglo
+console.log('arreglo', arreglo); // [...] arreglo
+
+// FILTER (Filtro arreglo)
+// enviamos expresión
+// devuelve los elementos que cumplen con esa condición en un nuevo arreglo
+const respuestaFilter = arreglo
+    .filter(
+        function (valorActual) {
+            return valorActual.nota > 8;
+        }
+    );
+
+console.log('respuestaFilter', respuestaFilter); // [...] nuevo arreglo
+console.log('arreglo', arreglo); // [...] arreglo
+
+// SOME -> expresión
+// devuelve boolean
+// ¿Hay alguna X que cumpla esta condición?
+// OR
+const respuestaSome = arreglo
+    .some(
+        function (valorActual) {
+            return valorActual.nota > 9;
+        }
+    );
+
+console.log('respuestaSome', respuestaSome); // true
+
+// EVERY -> expresión
+// devuelve boolean
+// ¿Todos los X cumplen esta condición?
+// AND
+const respuestaEvery = arreglo
+    .every(
+        function (valorActual) {
+            return valorActual.nota <= 20;
+        }
+    );
+console.log('respuestaEvery', respuestaEvery); // true
+
+// REDUCE izq -> Der
+// REDUCE RIGHT Der -> izq
+// 1) [1,2,5,6,2,4,5,6,7,8,3,1,2]
+// 2) 0 -> Variable (acumulador)
+// 3) devolvemos la Operación
+// Ej: Variable inicial en 100
+// 1_ 100 - 1 = 99
+// 2_ 99 - 2 = 97
+// 3_ 97 - 5 = 92
+// ..
+// EJ: Variable inicial en 0
+// 1_ 0 + 1 = 1
+// 2_ 1 + 2 = 3
+// 3_ 3 + 5 = 8
+// ...
+const respuestaReduce = arreglo
+    .reduce(
+        function (valorAcumulado, valorActual, indice, arreglo) {
+            return valorActual.nota + valorAcumulado;
+        },
+        0 // acumulador
+    );
+console.log('respuestaReduce', respuestaReduce); // 146
+console.log(respuestaReduce / arreglo.length); // 14.6
+
+// CONCATENAR OPERADORES
+arreglo.filter((a) => a.nota < 14)
+    .map((a) => a.nota + 1)
+    .some((a) => a < 14);
