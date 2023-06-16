93acc77564245de74d057e601ec3fa44803549af
Jonathan Puglla Lalvay <jonathan.puglla@epn.edu.ec>
2023-06-16 18:07:09 -0500

    Feat: 03 React
    React implementation

---
+++ 03-react/my-app/app/b_hola_mundo/page.tsx
@@ -0,0 +1,23 @@
+// /a_hola_mundo/page.tsx
+/*
+const a_componente = function () {
+    return (
+        <></>
+    )
+}
+*/
+
+/*
+export default a_componente
+const b_componente = () => {
+    return <></>
+}
+*/
+
+export default function page() {
+    return (
+        <>
+            <h1>Hola, desde b_hola_mundo</h1>
+        </>
+    )
+}
---
+++ 03-react/my-app/app/componentes/c_menu.component.tsx
@@ -0,0 +1,13 @@
+// app/componentes/c_menu.component.tsx
+
+export default function C_menuComponent() {
+    return (
+        <>
+            <ul>
+                <li><a href="/">Inicio</a></li>
+                <li><a href="/a_hola_mundo">A Hola Mundo</a></li>
+                <li><a href="/b_hola_mundo">B Hola Mundo</a></li>
+            </ul>
+        </>
+    )
+}
